01f35905073e100eb88dd66e6ba58c1691dbebc5
Tracy Stasch-Goode <t.stasch@abdn.ac.uk>
2022-11-27 18:26:31 +0000

    Add instructions for the technical challenge

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# its255a-tech-challenge
-PHP / UI technical challenge for the ITS255A Junior Software Developer position
+# ITS255A Junior Software Developer - Technical Challenge
+
+This technical challenge is intended for applicants to the University of Aberdeen post ITS255A Junior Software Developer.
+
+It will assess your ability to develop a usable and acecessible user interface in a PHP environment. This application is built in Laravel, a popular PHP framework.
+
+Please complete this technical challenge if you are interested in joining the Web Applications Development Team as a Junior Software Developer.
+
+## Pre-requisites
+
+The following software is required to complete this technical challenge:
+
+- Git
+- npm (NodeJS 16.x)
+- Composer 2.x
+- PHP 8.1.x
+- MariaDB 10.4.x
+- a modern web browser, such as Chrome
+
+**Tip for Windows users**: XAMPP is a free Apache distribution which contains PHP and MariaDB and is suited for this technical challenge. Alternatively, you can use Docker, WAMP or any other platform you wish (e.g. MAMP for macOS users).
+
+Composer, npm and Git will need to be installed separately. It is recommended to install these tools and set up your PHP/MariaDB instance before commencing the technical challenge.
+
+
+## Setup instructions
+
+1. Clone the repo.
+
+```sh
+$ git clone git@github.com:tea-mac/its255a-tech-challenge.git
+```
+
+2. Open a terminal at the root of the repo. Install PHP assets (make sure your PHP version is 8.1.x. You can check this by running `php --version` in the terminal).
+
+```sh
+$ composer install
+```
+
+3. Copy the `.env.example` file and rename it to `.env`. Pay attention to the settings for `DB_HOST`, `DB_PORT`, `DB_DATABASE` etc. You can change them if your database is running on a different port, but it is recommended to leave these settings as default and use 3306.
+
+4. Open the `.env` file and set the `APP_URL` to a suitable value (e.g. `http://127.0.0.1:8000` or whichever port you want to run your application from). Please note you won't be able to visit this URL until after the application is served in Step 9.
+
+5. Set the application key.
+
+```sh
+$ php artisan key:generate
+```
+
+6. Run the migrations and seed the database. This command will prompt you to create a database.
+
+```sh
+# Run the migrations
+$ php artisan migrate
+
+# When prompted to create a database, type 'y' and press enter
+
+# Seed the database
+$ php artisan db:seed
+```
+
+7. Install the third-party assets.
+
+```sh
+$ npm install
+```
+
+8. Open a new terminal and navigate to the root of the repo. Build the assets and start Vite.
+
+```sh
+$ npm run dev
+
+# Vite watches for code changes and dynamically rebuilds your views/assets etc.
+
+```
+
+9. Serve the application. You can specify a port using the ` --port=8000` flag, otherwise 8000 will be used by default.
+
+```sh
+$ php artisan serve
+```
+
+10. After serving the application, a URL will be displayed in the terminal. Copy and paste this into a web browser.  You should see a welcome page with the Laravel logo and some links to Laravel documentation.
+
+## Logging in
+
+A barebones application has been set up for you. On the welcome page, you will see two links in the top right-hand corner: "Log in" and "Register".
+
+1. Click "Register" and fill in the form. Note this application does not do mailbox verification, so feel free to set any email address you wish here.
+
+2. After completing registration, you will be automatically logged in and presented with a dashboard containing instructions to complete the technical challenge, along with some pointers.
+
+## Support
+
+In the first instance, please refer to documentation on [https://laravel.com/docs/9.x](https://laravel.com/docs/9.x).
+
+If you have any further questions please reach out to Tracy Stasch-Goode by email at [t.stasch-goode@abdn.ac.uk](t.stasch-goode@abdn.ac.uk).
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Home') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,63 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <p class="pb-4">
+                        If you've made it this far, congratulations! 🎉
+                    </p>
+                    <p class="pb-4">
+                        Please now add CRUD (create-read-update-delete) functionality to this application. A simple
+                        list view of all student records stored in the <span class="font-mono">students</span> table
+                        in the database has been created but needs a little work to make it more useful.
+                    </p>
+                    <ul class="list-disc list-inside pb-4">
+                        <li>
+                            Create a new student with all attributes as defined in the database. Check the migration
+                            (<span class="font-mono">app\database\migrations</span>) to determine what validation rules
+                            you should include in your form.
+                        </li>
+                        <li>
+                            Amend the list view to show all student information in a table. Currently,
+                            only <span class="font-mono">forename_1</span> and <span class="font-mono">surname</span>
+                            are displayed, but we want to see all information about each student.
+                        </li>
+                        <li>
+                            Update a student record.
+                        </li>
+                        <li>
+                            Delete a student's record from the database.
+                        </li>
+                    </ul>
+
+                    <p class="pb-4">
+                        When writing your code, please consider the following:
+                    </p>
+                    <ul class="list-disc list-inside pb-4">
+                        <li>
+                            Forms should have comprehensive <a class="hover:underline text-blue-500 hover:text-blue-700"
+                                href="https://laravel.com/docs/9.x/validation" target="_blank">server-side validation</a>.
+                        </li>
+                        <li>
+                            The <a class="hover:underline text-blue-500 hover:text-blue-700"
+                                href="https://www.php-fig.org/psr/psr-12/" target="_blank">PSR-12 coding standard</a>
+                            should be followed.
+                        </li>
+                        <li>
+                            Modern PHP features should be used, where appropriate.
+                        </li>
+                        <li>
+                            Markup should be valid and semantic.
+                        </li>
+                        <li>
+                            The application interface should adhere to the <a class="hover:underline text-blue-500 hover:text-blue-700"
+                                href="https://www.w3.org/TR/WCAG21/" target="_blank">WCAG 2.1 AA accessibility standard</a>
+                            as far as possible.
+                        </li>
+                    </ul>
+                    <p class="pb-4">
+                        The <a class="hover:underline text-blue-500 hover:text-blue-700"
+                            href="https://laravel.com/docs/9.x" target="_blank">Laravel documentation</a> is a good
+                        starting point if you are still getting familiar with Laravel.
+                    </p>
                 </div>
             </div>
         </div>
